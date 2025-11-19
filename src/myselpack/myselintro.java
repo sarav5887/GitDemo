f8a023165422026f1b31999997ec6a0acb168505
sarav <saravhasemail@gmail.com>
2025-11-19 14:43:50 +0530

    My first commit

---
+++ src/myselpack/myselintro.java
@@ -0,0 +1,42 @@
+package myselpack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class myselintro 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+	//String[]  name = {"lavina" , "sastika" , "saravana"};
+		
+		int[] a = {1,2,3,4,5,6};
+	
+	for (int i = 0; i < a.length; i++) 
+	{
+		
+		if (a[i] % 2 == 0) {
+			System.out.println(a[i]);
+			break;
+			
+		} else {
+System.out.println(a[i] + "No");
+		}
+		
+	}}}
+		
+		
+
+		
+		
+		
+		
+		
+	
+		
+
+	
